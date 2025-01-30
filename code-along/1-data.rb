108cfb059d1142f7f951dc9c21da4f6e13c825bf
Tanya Goley <tanya.goley@kellogg.northwestern.edu>
2025-01-30 00:36:12 +0000

    basics 1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,14 +5,35 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 2
+puts 5
+puts 5+2
+puts 5-2
+puts 5*2
+puts 5/2.0
 
 # Non-numbers
+puts "hello world"
+
+puts "Tacos are" + " Delish!"
+puts "tacos"*3
+puts "tacos" + 3.to_s
+puts "tacos" + " 3"
 
 # True, False
 
 # Nothing
+puts nil
 
 # Variables
+x = 10
+y = 3
+z = 13
+
+puts x+z
+food = "tacos"
+quantity = 3
+puts food*quantity
 
 # Combine strings and variables
 
